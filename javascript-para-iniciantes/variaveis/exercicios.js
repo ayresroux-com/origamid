59d0c3ebfaa7a9a1bcc694bfbcc520bf3f17548c
Ayres Roux <contatoayresroux@gmail.com>
2022-07-29 20:39:14 -0300

    0201 Variáveis

---
+++ javascript-para-iniciantes/variaveis/exercicios.js
@@ -0,0 +1,19 @@
+// Declarar uma variável com o seu nome
+let nome = "Ayres Roux";
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+let idade = 29;
+console.log(idade);
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+
+// Atrubuir valor a sua comida favorita
+comidaFavorita = "Macarrão";
+console.log(comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+let email, whatsapp, site, facebook, instagram;
+console.log(email, whatsapp, site, facebook, instagram);
---
+++ javascript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,36 @@
+var nome = "André";
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Rafael",
+  cidade = "Rio";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
+
+// console.log(aindaNaoDefine);
+/*
+Comentário com
+mais de uma 
+linha
+*/
+
+console.log(comida);
+var comida = "Pizza";
+console.log(comida);
+
+// const time = "Vasco";
+// time = "Flamengo";
+
+// console.log(time);
