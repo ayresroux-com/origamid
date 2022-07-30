e3e7c4edcc5eae8109f3224f035c351720f842af
Ayres Roux <contatoayresroux@gmail.com>
2022-07-29 22:41:08 -0300

    0203 Números e Operadores 1

---
+++ javascript-para-iniciantes/numeros-e-operadores/script.js
@@ -0,0 +1,21 @@
+var idade = 23;
+console.log(idade);
+
+var soma = 10 + 5;
+console.log(soma);
+
+var subtracao = 10 - 5;
+console.log(subtracao);
+
+var divisao = 10 / 5;
+console.log(divisao);
+
+var multiplicacao = 10 * 5;
+console.log(multiplicacao);
+
+var modulo = 10 % 6;
+console.log(modulo);
+
+var testeNaN = "Isso é 100" / 2;
+console.log(testeNaN);
+console.log(isNaN(testeNaN));
