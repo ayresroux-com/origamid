f06c8d8c8c28fde78cd3bb4750d2764055ab1c9d
Ayres Roux <contatoayresroux@gmail.com>
2022-07-29 21:02:54 -0300

    0202 Tipos de Dados

---
+++ javascript-para-iniciantes/tipos-de-dados/exercicios.js
@@ -0,0 +1,25 @@
+// Declare uma variável contendo uma string
+var nome = "Ayres";
+console.log(typeof nome);
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = "2022";
+console.log(typeof ano);
+
+// Declare uma variável com a sua idade
+var idade = 29;
+console.log(typeof idade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Ayres";
+var sobrenome = "Roux";
+var nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+
+// Cloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nomeCompleto);
---
+++ javascript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,33 @@
+var nome = "André";
+var idade = 23;
+var time;
+var time = null;
+var simbolo = Symbol();
+
+console.log(typeof simbolo);
+
+var nome = "André";
+var sobrenome = "Rafael";
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = "Romário fez " + gols + " gols";
+
+console.log(frase);
+
+var ano = "2018";
+var mes = 8;
+
+console.log(ano + mes);
+
+var frase1 = 'Esse é o "melhor" jogo';
+
+console.log(frase1);
+
+console.log("Isso mesmo");
+
+var frase2 = `Romário fez ${gols * 2} gols`;
+
+console.log(frase2);
